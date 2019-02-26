e0264910b9519eef21af68dd629713b871a7e0d7
vagrant <vagrant@vagrant.vm>
2019-02-26 00:32:47 +0000

    added logout route and account

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, BlogForm
 from flaskblog.model import User, Post
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route('/')
@@ -13,6 +13,8 @@ def homepage():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('homepage'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -25,15 +27,28 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('homepage'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('homepage'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('homepage'))
         else:
             flash('Login unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
 
 
 # @app.route('/create', methods=['GET', 'POST'])
